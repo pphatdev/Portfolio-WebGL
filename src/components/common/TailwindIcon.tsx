515171f5e22d889986da596dc8dc5749afb43e95
pphatdev <leatsophat02@gmail.com>
2026-03-01 16:35:21 +0700

    feat: add TailwindIcon component for customizable SVG icon support

---
+++ src/components/common/TailwindIcon.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+interface TailwindIconProps {
+    className?: string;
+    style?: React.CSSProperties;
+}
+
+const TailwindIcon: React.FC<TailwindIconProps> = ({ className, style }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className={className}
+            style={style}
+        >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M11.667 6c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 2 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968m-4 6.5c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 1.975 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968" />
+        </svg>
+    );
+};
+
+export default TailwindIcon;
